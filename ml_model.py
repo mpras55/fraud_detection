a395039ad2188ca852c0f9401940e9aded976e2f
mpras55 <manjunath.bp@gmail.com>
2022-06-08 16:24:04 -0600

    added 3 new models in ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,11 @@
 
 print("This is for ML model")
+
+def model1():
+    print("This is ML model 1")
+
+def model2():
+    print("This is ML model 2")
+
+def model3():
+    print("This is ML model 3")
